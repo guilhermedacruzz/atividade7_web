709c5b55b63dc2c093ddb95b3a1edfa0aac3713c
indexGuilherme <guidacruz2004@gmail.com>
2021-10-04 11:39:47 -0300

    learning how to use canvas in differents files

---
+++ scripts/teste.js
@@ -0,0 +1,9 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);

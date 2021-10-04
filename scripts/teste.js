de3264b042a96b0a08060a1516fad8277942803d
indexGuilherme <guidacruz2004@gmail.com>
2021-10-04 12:51:20 -0300

    testing the game loop

--- scripts/teste.js
+++ scripts/teste.js
@@ -1,5 +1,3 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
 
 let width = 400;
 let height = 400;
@@ -10,13 +8,26 @@ var cols = width/w;
 var rows = height/h;
 
 function startMaze() {
-    myMaze();
+    myMaze.start();
 }
 
-var myMaze = function() {
+var myMaze = {
+
+    canvas : document.getElementById("myCanvas"),
+    start : function() {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }    
+}
+
+function draw() {
     for(let i = 0; i < rows; i++) {
         for(let f = 0; f < cols; f++) {
-            ctx.fillRect(i * w, f * h, w - 1, h -1);
+            myMaze.context.fillRect(i * w, f * h, w - 1, h -1);
         }
     }
 }
+
+setInterval(draw, 10);

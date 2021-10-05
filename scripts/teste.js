e37e78a88cfd0dcaf0d9cc6bdd2657147d110376
indexGuilherme <guidacruz2004@gmail.com>
2021-10-05 18:23:11 -0300

    relacionado a renderização das linhas com um status

--- scripts/teste.js
+++ scripts/teste.js
@@ -2,27 +2,36 @@ class Cell {
     constructor(i, f) {
         this.i = i;
         this.f = f;
+
+        //            top   right  bottom  left
+        this.walls = [true, true,  true, true];
     }
 
     show(context, scale) {
-        var x = this.i * scale;
+        var x = this.i * scale; 
         var y = this.f * scale;
 
-        context.moveTo(x, y);
-        context.lineTo(x + scale, y);
+        if(this.walls[0]) {
+            context.moveTo(x, y);
+            context.lineTo(x + scale, y); 
+        }
 
-        context.moveTo(x + scale, y);
-        context.lineTo(x + scale, y + scale);
+        if(this.walls[1]) {
+            context.moveTo(x + scale, y);
+            context.lineTo(x + scale, y + scale);
+        }
 
-        context.moveTo(x + scale, y + scale);
-        context.lineTo(x, y + scale);
+        if(this.walls[2]) {
+            context.moveTo(x + scale, y + scale);
+            context.lineTo(x, y + scale);
+        }
 
-        context.moveTo(x, y + scale);
-        context.lineTo(x, y);
+        if(this.walls[3]) {
+            context.moveTo(x, y + scale);
+            context.lineTo(x, y);
+        }
 
         context.stroke();
-
-        //context.fillRect(this.i * scaleH, this.f * scaleW, scaleW - 1, scaleH - 1);
     }
 }
 
